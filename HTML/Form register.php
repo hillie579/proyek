8a67bf45d287ede266aabef75facdd671f5cf6a2
hillie579 <lanpha579@gmail.com>
2024-05-29 11:10:57 +0700

    Add files via upload

---
+++ HTML/Form register.php
@@ -0,0 +1,36 @@
+<div class="container">
+        <form id="login-form">
+            <h2>Register</h2>
+            <div class="input-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required>
+            </div>
+            <div class="input-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="input-group">
+                <label for="Email">Email</label>
+                <input type="text" id="Email" name="Email" required>
+            </div>
+            <div class="input-group">
+                <label for="Role">Role</label>
+                <select class="input-group">
+                    <option>Tank</option>
+                    <option>Marksman</option>
+                    <option>Jungler</option>
+                    <option>Fighter</option>
+                    <option>Mage</option>
+                </select>
+            </div>
+            <div class="input-group">
+                <label for="gender">Gender</label>
+                <input type="text" id="gender" name="gender" required>
+            </div>
+            <div class="input-group">
+                <label for="hobi">Hobi</label>
+                <input type="text" id="hobi" name="hobi" required>
+            </div>
+            <button type="submit">Login</button>
+            <link rel = "stylesheet" href="style.css">
+        </form>
---
+++ HTML/Website Hillie.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!-- saved from url=(0058)file:///C:/xampp/htdocs/LoeiszMiquelWangUKK/HTML/menu.html -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <link rel = "stylesheet" href="style.css">
+ <title>Website Hillie</title>
+</head>
+<body>
+ <header>
+  <h1>Selamat Datang di Website Hillie</h1>
+ </header>
+ <nav>
+  <ul>
+   <li><a href="Form login.php">login</a></li>
+   <li><a href="Form register.php">register</a></li>
+  </ul>
+ </nav>
+ <section>
+  <h3>Menu Terbaru</h3>
+ </section>
+ <footer>
+  <p>Menu tak jelas</p>
+ </footer>
+
+</body></html>
